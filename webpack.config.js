034861808874bc4e6bafda06f62e064d99a38170
Matthew Burgess <boog397@gmail.com>
2024-06-11 00:11:11 -0500

    Add HTML template to webpack watch list for automatic change detection

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
+        watchFiles: ["src/*.html"]
     },
     plugins: [
         new HtmlWebpackPlugin({
