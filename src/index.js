af5913419df17b77a61033be9de09edb09a45a85
Matthew Burgess <boog397@gmail.com>
2024-06-17 23:44:03 -0500

    Add ability to switch between home and about pages by clicking on their corresponding navigation buttons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import aboutPage from './about.js';
 import './styles.css';
 
 const divContent = document.querySelector('#content');
+const btnHome = document.querySelector('#btn-home');
+const btnAbout = document.querySelector('#btn-about');
 
-// homepage.create(divContent);
-aboutPage.create(divContent);
+const navigationEvents = new function() {
+    
+    const loadHomePage = () => {
+        clearPage();
+        homepage.create(divContent);
+    }
+
+    const loadAboutPage = () => {
+        clearPage();
+        aboutPage.create(divContent);
+    }
+
+    const clearPage = () => {
+        while(divContent.lastChild) {
+            divContent.removeChild(divContent.lastChild);
+        }
+    }
+
+    return { loadHomePage, loadAboutPage };
+}
+
+btnHome.addEventListener('click', navigationEvents.loadHomePage);
+btnAbout.addEventListener('click', navigationEvents.loadAboutPage);
+
+navigationEvents.loadHomePage();
