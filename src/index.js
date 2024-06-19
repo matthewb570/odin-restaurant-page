9bf7ba037a6816c5d017366b48b5a0588d5466ef
Matthew Burgess <boog397@gmail.com>
2024-06-19 17:39:12 -0500

    Add ability to load the menu page by clicking on the menu button in the navigation bar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import homePage from './home.js';
+import menuPage from './menu.js';
 import aboutPage from './about.js';
 import './styles.css';
 
 const divContent = document.querySelector('#content');
 const btnHome = document.querySelector('#btn-home');
+const btnMenu = document.querySelector('#btn-menu');
 const btnAbout = document.querySelector('#btn-about');
 
 const navigationEvents = new function() {
@@ -11,6 +13,11 @@ const navigationEvents = new function() {
     const loadHomePage = () => {
         clearPage();
         homePage.create(divContent);
+    }
+
+    const loadMenuPage = () => {
+        clearPage();
+        menuPage.create(divContent);
     }
 
     const loadAboutPage = () => {
@@ -24,10 +31,11 @@ const navigationEvents = new function() {
         }
     }
 
-    return { loadHomePage, loadAboutPage };
+    return { loadHomePage, loadMenuPage, loadAboutPage };
 }
 
 btnHome.addEventListener('click', navigationEvents.loadHomePage);
+btnMenu.addEventListener('click', navigationEvents.loadMenuPage);
 btnAbout.addEventListener('click', navigationEvents.loadAboutPage);
 
 navigationEvents.loadHomePage();
