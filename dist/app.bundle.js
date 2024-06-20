0e45cc3dbfaf7cdf17119b860d42867955052ecf
Matthew Burgess <boog397@gmail.com>
2024-06-19 19:20:15 -0500

    Initial dist subtree commit

---
+++ dist/app.bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hello world!');
+console.log('Again');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmJ1bmRsZS5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0EscUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9vZGluLXJlc3RhdXJhbnQtcGFnZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSGVsbG8gd29ybGQhJyk7XG5jb25zb2xlLmxvZygnQWdhaW4nKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
